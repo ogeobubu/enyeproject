12626856b92e6384829027c1e5515b33eb86e673
Oge Obubu <ogeobubu@gmail.com>
2021-01-12 09:06:06 +0100

    changed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
+import { Alert } from "react-bootstrap";
+
 function App() {
   return (
     <div className="container">
+      <Alert variant="success">
+        <Alert.Heading>
+          Hi there!👋 I appreciate you for checking this out
+        </Alert.Heading>
+        <p>
+          This is built using JavaScript to be precise React.js and
+          React-Bootstrap as the CSS library.
+        </p>
+      </Alert>
       <Header />
       <Footer />
     </div>
